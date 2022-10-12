ace541f89a7296c7a16695920c352fcba1503975
sabhaya bhumit <sabhayabhumit@gmail.com>
2022-10-12 23:38:07 +0530

    Media Booster In Intro Screen

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:media_booster_3/global.dart';
+import 'package:media_booster_3/screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => home(),
+        'screen': (context) => screen(),
+      },
+    ),
+  );
+}
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Imgae You Like"), centerTitle: true),
+      body: ListView.builder(
+          itemCount: global.all_image.length,
+          itemBuilder: (context, i) => Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Stack(
+                      alignment: Alignment(-0.9, 1),
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              Navigator.of(context).pushNamed('screen',
+                                  arguments: global.all_image[i]);
+                            });
+                          },
+                          child:
+                              Image.network("${global.all_image[i]['image']}"),
+                        ),
+                        Text(
+                          "${global.all_image[i]['name']}",
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+    );
+  }
+}
